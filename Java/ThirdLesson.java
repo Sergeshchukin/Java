089fbce1d126478150a22fa6a3e9b3fb1a7ab8c8
Sergeshchukin <94902372+Sergeshchukin@users.noreply.github.com>
2021-12-18 20:31:11 +0300

    Update ThirdLesson.java

--- Java/ThirdLesson.java
+++ Java/ThirdLesson.java
@@ -31,6 +31,7 @@ public class ThirdLesson {
         int[] metod;
         metod = Mass(12, 5);
         System.out.println(Arrays.toString(metod));
+    }
 
         // 1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
         public static void InvertArr(int[] arr) {
@@ -48,7 +49,7 @@ public class ThirdLesson {
 
         public static void FillArr(int[] array) {
             for (int i = 0; i < array.length; i++) {
-                array[i] = (i);
+                array[i] = i;
             }
         }
 // 3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
@@ -93,5 +94,4 @@ public class ThirdLesson {
             }
             return metod;
         }
-    }
 }
