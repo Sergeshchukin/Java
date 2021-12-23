f04548fd4a24aea962fbd0b8bf59dfbe11aedbda
Sergeshchukin <94902372+Sergeshchukin@users.noreply.github.com>
2021-12-23 15:20:13 +0300

    Update ThirdLesson.java

--- Java/ThirdLesson.java
+++ Java/ThirdLesson.java
@@ -12,29 +12,29 @@ public class ThirdLesson {
     public static void main(String[] agrs) {
 //1
         int[] arr = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
-        InvertArr(arr);
+        invertarr(arr);
         System.out.println(Arrays.toString(arr));
 //2
         int[] array;
         array = new int[100];
-        FillArr(array);
+        fillarr(array);
         System.out.println(Arrays.toString(array));
 //3
         int[] multiarr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        MultiArr(multiarr);
+        multiArr(multiarr);
         System.out.println(Arrays.toString(multiarr));
 //4
         int[][] matrix;
         matrix = new int[3][3];
-        Massive(matrix);
+        massive(matrix);
 //5
         int[] metod;
-        metod = Mass(12, 5);
+        metod = mass(12, 5);
         System.out.println(Arrays.toString(metod));
     }
 
     // 1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
-    public static void InvertArr(int[] arr) {
+    public static void invertarr(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == 1) {
                 arr[i] = 0;
@@ -47,14 +47,14 @@ public class ThirdLesson {
 
 // 2. Задать пустой целочисленный массив длиной 100. С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 … 100;
 
-    public static void FillArr(int[] array) {
+    public static void fillarr(int[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] = (i + 1);
         }
     }
 // 3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
 
-    public static void MultiArr(int[] multiarr) {
+    public static void multiArr(int[] multiarr) {
         for (int i = 0; i < multiarr.length; i++) {
             if (multiarr[i] < 6) {
                 multiarr[i] *= 2;
@@ -66,7 +66,7 @@ public class ThirdLesson {
 // Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны,
 // то есть [0][0], [1][1], [2][2], …, [n][n];
 
-    public static void Massive(int[][] matrix) {
+    public static void massive(int[][] matrix) {
         int n = matrix.length;
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
@@ -87,7 +87,7 @@ public class ThirdLesson {
 //5 Написать метод, принимающий на вход два аргумента: len и initialValue, и возвращающий одномерный массив
 // типа int длиной len, каждая ячейка которого равна initialValue;
 
-    public static int[] Mass(int len, int initialValue) {
+    public static int[] mass(int len, int initialValue) {
         int[] metod;
         metod = new int[len];
         for (int i = 0; i < metod.length; i++) {
