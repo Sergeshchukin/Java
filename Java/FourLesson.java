b6a45c426ce9ed17e3f029587f16171f1d35864e
Sergeshchukin <94902372+Sergeshchukin@users.noreply.github.com>
2021-12-28 17:10:35 +0300

    Update FourLesson.java

--- Java/FourLesson.java
+++ Java/FourLesson.java
@@ -1,181 +1,126 @@
 /*
 *
-*  @author Sergey Shchukin
-*  @version 22.12.2021
 *
- */
+* @author Sergey Shchukin
+* @version 28.12.2021
+*
+*/
 
-
-
-
-
-
-
-
-import java.util.*;
+import java.util.Scanner;
+import java.util.Random;
 
 public class FourLesson {
+    public static char[][] map;
+    public static final int SIZE = 3;
+    public static final int DOTS_TO_WIN = 3;
+    public static final char DOT_EMPTY = '.';
+    public static final char DOT_X = 'X';
+    public static final char DOT_O = 'O';
+    public static void main(String[] agrs) {
+        initMap();
+        printMap();
 
-    static String[] board;
-    static String turn;
-
-    static String checkWinner()
-    {
-        for (int a = 0; a < 8; a++) {
-            String line = null;
-
-            switch (a) {
-                case 0:
-                    line = board[0] + board[1] + board[2];
-                    break;
-                case 1:
-                    line = board[3] + board[4] + board[5];
-                    break;
-                case 2:
-                    line = board[6] + board[7] + board[8];
-                    break;
-                case 3:
-                    line = board[0] + board[3] + board[6];
-                    break;
-                case 4:
-                    line = board[1] + board[4] + board[7];
-                    break;
-                case 5:
-                    line = board[2] + board[5] + board[8];
-                    break;
-                case 6:
-                    line = board[0] + board[4] + board[8];
-                    break;
-                case 7:
-                    line = board[2] + board[4] + board[6];
-                    break;
-            }
-            //For X winner
-            if (line.equals("XXX")) {
-                return "X";
-            }
-
-            // For O winner
-            else if (line.equals("OOO")) {
-                return "O";
-            }
-        }
-
-        for (int a = 0; a < 9; a++) {
-            if (Arrays.asList(board).contains(
-                    String.valueOf(a + 1))) {
+        while (true) {
+            humanTurn();
+            printMap();
+            if (isWinner(DOT_X)) {
+                System.out.println("YOU WON");
                 break;
             }
-            else if (a == 8) {
-                return "draw";
+            if (isMapFull()) {
+                System.out.println("DRAW");
+                break;
+            }
+            aiTurn(); // ход робота
+            printMap();
+            if (isWinner(DOT_O)) {
+                System.out.println("AI WON");
+                break;
+            }
+            if (isMapFull()) {
+                System.out.println("DRAW");
+                break;
             }
         }
-
-
-        System.out.println(
-                turn + "'s turn; enter a slot number to place "
-                        + turn + " in:");
-        return null;
+        System.out.println("GAME OVER");
     }
 
-    // To print out the board.
-    /* |---|---|---|
-       | 1 | 2 | 3 |
-       |-----------|
-       | 4 | 5 | 6 |
-       |-----------|
-       | 7 | 8 | 9 |
-       |---|---|---|*/
-
-    static void printBoard()
-    {
-        System.out.println("|---|---|---|");
-        System.out.println("| " + board[0] + " | "
-                + board[1] + " | " + board[2]
-                + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[3] + " | "
-                + board[4] + " | " + board[5]
-                + " |");
-        System.out.println("|-----------|");
-        System.out.println("| " + board[6] + " | "
-                + board[7] + " | " + board[8]
-                + " |");
-        System.out.println("|---|---|---|");
+    public static void initMap() {
+        map = new char[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                map[i][j] = DOT_EMPTY;
+            }
+        }
     }
 
-    public static void main(String[] args)
-    {
-        Scanner in = new Scanner(System.in);
-        board = new String[9];
-        turn = "X";
-        String winner = null;
-
-        for (int a = 0; a < 9; a++) {
-            board[a] = String.valueOf(a + 1);
+    public static void printMap() {
+        for (int i = 0; i <= SIZE; i++) {
+            System.out.print(i + " ");
         }
-
-        System.out.println("Welcome to 3x3 Tic Tac Toe.");
-        printBoard();
-
-        System.out.println(
-                "X will play first. Enter a slot number to place X in:");
-
-        while (winner == null) {
-            int numInput;
-
-            // Exception handling.
-            // numInput will take input from user like from 1 to 9.
-            // If it is not in range from 1 to 9.
-            // then it will show you an error "Invalid input."
-            try {
-                numInput = in.nextInt();
-                if (!(numInput > 0 && numInput <= 9)) {
-                    System.out.println(
-                            "Invalid input; re-enter slot number:");
-                    continue;
-                }
+        System.out.println();
+        for (int i = 0; i < SIZE; i++) {
+            System.out.print((i + 1) + " ");
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(map[i][j] + " ");
             }
-            catch (InputMismatchException e) {
-                System.out.println(
-                        "Invalid input; re-enter slot number:");
-                continue;
-            }
-
-            // This game has two player x and O.
-            // Here is the logic to decide the turn.
-            if (board[numInput - 1].equals(
-                    String.valueOf(numInput))) {
-                board[numInput - 1] = turn;
-
-                if (turn.equals("X")) {
-                    turn = "O";
-                }
-                else {
-                    turn = "X";
-                }
-
-                printBoard();
-                winner = checkWinner();
-            }
-            else {
-                System.out.println(
-                        "Slot already taken; re-enter slot number:");
-            }
+            System.out.println();
         }
-
-        // If no one win or lose from both player x and O.
-        // then here is the logic to print "draw".
-        if (winner.equalsIgnoreCase("draw")) {
-            System.out.println(
-                    "It's a draw! Thanks for playing.");
-        }
-
-        // For winner -to display Congratulations! message.
-        else {
-            System.out.println(
-                    "Congratulations! " + winner
-                            + "'s have won! Thanks for playing.");
-        }
+        System.out.println();
     }
+
+    public static void humanTurn() {
+        Scanner scanner = new Scanner(System.in);
+        int x, y;
+        do {
+            System.out.println("input X Y");
+            x = scanner.nextInt() - 1;
+            y = scanner.nextInt() - 1;
+        } while (!isCellValid(x, y));
+        map[y][x] = DOT_X;
+    }
+
+    public static void aiTurn() {
+        Random random = new Random();
+        int x, y;
+        do {
+            x = random.nextInt(SIZE);
+            y = random.nextInt(SIZE);
+        } while (!isCellValid(x, y));
+        System.out.println();
+        map[y][x] = DOT_O;
+    }
+
+    public static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) return false;
+        if (map[y][x] == DOT_EMPTY) return true; //
+        return false;
+    }
+
+    public static boolean isWinner(char symb) {
+        boolean first = true;
+        boolean second = true;
+        for(int i = 0; i < SIZE; i++)
+        {
+            first &= map[i][i]==symb;
+            second &= map[i][(SIZE-1)-i] ==symb;
+            if (map[i][1]==symb&&map[i][2]==symb&&map[i][3]==symb&&map[i][4]==symb) return true;
+            if (map[i][0]== symb&&map[i][1]==symb&&map[i][2]==symb&&map[i][3]==symb) return true;
+            if (map[1][i]==symb&&map[2][i]==symb&&map[3][i]==symb&&map[4][i]==symb) return true;
+            if (map[0][i]== symb&&map[1][i]==symb&&map[2][i]==symb&&map[3][i]==symb) return true;
+        }
+        return first||second;
+    }
+
+    public static boolean isMapFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][j] == DOT_EMPTY) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
 }
