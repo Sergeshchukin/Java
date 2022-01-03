637b59017312cff571b685380b932ada4f195b19
Sergeshchukin <94902372+Sergeshchukin@users.noreply.github.com>
2022-01-03 14:50:34 +0300

    Update SixLesson.java

--- Java/SixLesson.java
+++ Java/SixLesson.java
@@ -21,69 +21,70 @@ public class SixLesson {
 
     }
 
+}
 
-    public abstract class Animal {
-        protected String name;
-        protected String color;
-        protected int age;
-        protected int obstacles;
-        int maxMove;
-        int maxDrift;
+abstract class Animal {
+    protected String name;
+    protected String color;
+    protected int age;
+    protected int obstacles;
+    int maxMove;
+    int maxDrift;
 
 
-        Animal(String name, String color, int age, int obstacles) {
-            this.name = name;
-            this.color = color;
-            this.age = age;
-            this.obstacles = obstacles;
-            this.maxMove = 0;
-            this.maxDrift = 0;
-            countAnimal++;
-
-        }
-
-        public static int countAnimal = 0;
-
-        void move() {
-            if (maxMove <= 0)
-                System.out.println(name + " cant run");
-            else if (maxMove >= obstacles)
-                System.out.println(name + " run " + obstacles + "m ");
-            else
-                System.out.println("for " + name + " to long distance");
-
-        }
-
-        void drift() {
-            if (maxDrift <= 0)
-                System.out.println(name + " cant swim");
-            else if (maxDrift >= obstacles)
-                System.out.println(name + " swim " + obstacles + "m ");
-            else
-                System.out.println("for " + name + " to long distance");
-        }
-    }
-
-    public class Cat extends Animal {
-        Cat(String name, String color, int age, int obstacles) {
-            super(name, color, age, obstacles);
-            this.maxMove = 500;
-            this.maxDrift = 2;
-            countCat++;
-        }
-
-        public static int countCat = 0;
+    Animal(String name, String color, int age, int obstacles) {
+        this.name = name;
+        this.color = color;
+        this.age = age;
+        this.obstacles = obstacles;
+        this.maxMove = 0;
+        this.maxDrift = 0;
+        countAnimal++;
 
     }
 
-    public class Dog extends Animal {
-        Dog(String name, String color, int age, int obstacles) {
-            super(name, color, age, obstacles);
-            this.maxMove = 500;
-            this.maxDrift = 10;
-            countDog++;
-        }
+    public static int countAnimal = 0;
 
-        static int countDog = 0;
+    void move() {
+        if (maxMove <= 0)
+            System.out.println(name + " cant run");
+        else if (maxMove >= obstacles)
+            System.out.println(name + " run " + obstacles + "m ");
+        else
+            System.out.println("for " + name + " to long distance");
+
     }
+
+    void drift() {
+        if (maxDrift <= 0)
+            System.out.println(name + " cant swim");
+        else if (maxDrift >= obstacles)
+            System.out.println(name + " swim " + obstacles + "m ");
+        else
+            System.out.println("for " + name + " to long distance");
+    }
+}
+
+class Cat extends Animal {
+    Cat(String name, String color, int age, int obstacles) {
+        super(name, color, age, obstacles);
+        this.maxMove = 500;
+        this.maxDrift = 2;
+        countCat++;
+    }
+
+    public static int countCat = 0;
+
+}
+
+
+class Dog extends Animal {
+    Dog(String name, String color, int age, int obstacles) {
+        super(name, color, age, obstacles);
+        this.maxMove = 500;
+        this.maxDrift = 10;
+        countDog++;
+    }
+
+    static int countDog = 0;
 }
